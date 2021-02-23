8888a8f6754a8055ee2c6e93cc1b016ae0b39835
mutua-mutinda <mutuamutinda@outlook.com>
2021-02-23 11:04:51 +0300

    impemented two api call - displayed monsters name and image

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import "./App.css";
+import { CardList } from "./components/card-list/card-list";
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      searchField: "",
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
+  }
+  render() {
+    return (
+      <div className="App">
+        <input
+          type="search"
+          placeholder="search monster"
+          onChange={(e) => this.setState({ searchField: e.target.value })}
+        />
+        <CardList monsters={this.state.monsters} />
+      </div>
+    );
+  }
+}
+
+export default App;
